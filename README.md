1f53b042096b19a45b3373ea20371a7fa0d62c74
OsagieG <dgodstand@gmail.com>
2025-08-03 18:15:39 +0100

    shed

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 Switch between multiple GitHub accounts without dealing with SSH keys
 
 - This tool is strictly global and does not alter the GitHub user settings
-  within Code Editors/IDEs such as Visual Studio Code or JetBrains products.
+  within Code Editors/IDEs such as Visual Studio or JetBrains products.
 
 - I used a Zsh script because it is my preferred choice,
   but you can modify the script to use Bash, POSIX, or
   any other shell of your choice.
+
+  ![shed](https://github.com/OsagieDG/shed/blob/main/shed.png)
--- shed.sh
+++ shed.sh
@@ -20,13 +20,18 @@ fi
 git config --global user.name "$required_username"
 git config --global user.email "$required_email"
 
-# If there are previously stored credentials for GitHub
-# in your keychain, this removes existing credentials for GitHub.
-security delete-internet-password -l "github.com"
+# Remove Github credentials for macOS Keychain
+if [[ "$(uname)" == "Darwin" ]]; then
+  security delete-internet-password -l "github.com"
+else
+  echo "Skipping macOS keychain command on non-macOS system"
+fi
 
 # Unsets credential helper configurations
 git config --global --unset credential.helper
 git config --system --unset credential.helper
+
+echo "\n"
 
 echo "GitHub account switched successfully."
 
